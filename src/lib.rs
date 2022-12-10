e43cb1766e6a798a93561740800470f77c531da3
cleilson pereira <marcialwushu@hotmail.com>
2022-12-10 17:36:01 -0300

    :fire: Add Yew Basic TodoApp

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+struct TodoApp {
+    link: ComponentLink<Self>,
+    todos: Option<Vec<Todo>>,
+    fetch_task: Option<FetchTask>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Deserialize, Clone, PartialEq, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Todo {
+    pub user_id: u64,
+    pub id: u64,
+    pub title: String,
+    pub completed: bool,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+enum Msg {
+    MakeReq,
+    Resp(Result<Vec<Todo>, anyhow::Error>),
+}
+
+impl Component for TodoApp {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            todos: None,
+            fetch_task: None,
+        }
     }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div class=classes!("todo")>
+                ...
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<TodoApp>::new().mount_to_body();
 }
